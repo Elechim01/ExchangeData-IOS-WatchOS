8993ec11cd85252be28472a69ab64234740fdcb6
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2023-04-29 22:48:19 +0200

    Initial Commit

---
+++ ExchangeData Watch App/ExchangeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeDataApp.swift
+//  ExchangeData Watch App
+//
+//  Created by Michele Manniello on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeData_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExchangeData/ExchangeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeDataApp.swift
+//  ExchangeData
+//
+//  Created by Michele Manniello on 29/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
